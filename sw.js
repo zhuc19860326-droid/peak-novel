00e68e488acd68968870c00785653d1c4b758c1b
zc <zhuc19860326@gmail.com>
2026-09-07 23:08:26 +0800

    Initial commit: PEAK interactive novel

---
+++ sw.js
@@ -0,0 +1,31 @@
+// PEAK 交互式小说 - Service Worker
+const CACHE_NAME = 'peak-novel-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => 
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
